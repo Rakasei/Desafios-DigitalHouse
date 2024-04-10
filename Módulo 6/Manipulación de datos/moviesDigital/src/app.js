a042e2626c2d529478d5b0a62df4294b358bbdd4
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-04-10 19:40:33 -0300

    Agrego desafio

---
+++ Módulo 6/Manipulación de datos/moviesDigital/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const moviesRoutes = require('./routes/moviesRoutes');
+const genresRoutes = require('./routes/genresRoutes');
+const actorsRoutes = require('./routes/actorsRoutes');
+const app = express();
+
+// view engine setup
+app.set('views', path.resolve(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.use('/', indexRouter);
+app.use(moviesRoutes);
+app.use(genresRoutes);
+app.use(actorsRoutes);
+
+app.listen('3001', () => {
+    console.log( 'Servidor corriendo en el puerto 3001'); 
+    console.log('\x1b[36m%s\x1b[0m','http://localhost:3001/'); 
+  });
+  
