d9ff0ebf5c30cb1d05717939781e68b76bfed96a
Rodrigo Ezequiel Ruiz <98621578+Rakasei@users.noreply.github.com>
2024-04-13 18:54:31 -0300

    Agrego módulos locales

---
+++ Módulo 2/Variables y tipos de datos/microDesafio1.js
@@ -0,0 +1,36 @@
+// Micro-Desafio 1 - Control de venta de boletos
+
+//Variables
+let nombre = "Pepe";
+let apellido = "Gómez";
+let numeroDeTelefono = 1145983536;
+let esSocio = true;
+let fechaDelPartido = "11/12/2023";
+let horaDelPartido = "22:00";
+let numeroDeSilla = 455;
+let precioBoleto = 19999.99;
+
+// Mensajes en consola
+console.log(nombre);
+console.log(typeof nombre);
+
+console.log(apellido);
+console.log(typeof apellido);
+
+console.log(numeroDeTelefono);
+console.log(typeof numeroDeTelefono);
+
+console.log(socio);
+console.log(typeof socio);
+
+console.log(fechaDelPartido);
+console.log(typeof fechaDelPartido);
+
+console.log(horaDelPartido);
+console.log(typeof horaDelPartido);
+
+console.log(numeroDeSilla);
+console.log(typeof numeroDeSilla);
+
+console.log(precioBoleto);
+console.log(typeof precioBoleto);
